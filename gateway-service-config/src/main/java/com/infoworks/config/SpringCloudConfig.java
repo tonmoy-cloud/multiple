039a2ca6835c0a6e6942065c1a821cf8af4552ad
Towhidul Islam <towhidul.islam@nagad.com.bd>
2021-05-29 21:53:07 +0600

    adding global-filter example

--- gateway-service-config/src/main/java/com/infoworks/config/SpringCloudConfig.java
+++ gateway-service-config/src/main/java/com/infoworks/config/SpringCloudConfig.java
@@ -1,12 +1,24 @@
 package com.infoworks.config;
 
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class SpringCloudConfig {
+
+    @Bean
+    public GlobalFilter globalFilter() {
+        return (exchange, chain) -> {
+            System.out.println("Pre Global filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                System.out.println("Post Global filter");
+            }));
+        };
+    }
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
