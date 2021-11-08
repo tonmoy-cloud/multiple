4c0ae140313904a71ddb80d41a6757a0b548e0f0
Towhidul Islam <m.towhid.islam@gmail.com>
2021-11-08 19:06:27 +0600

    updated

--- first-service/src/main/java/com/infoworks/controller/FirstController.java
+++ first-service/src/main/java/com/infoworks/controller/FirstController.java
@@ -1,6 +1,9 @@
 package com.infoworks.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employee")
@@ -10,4 +13,16 @@ public class FirstController {
 	public String test() {
 		return "Hello JavaInUse Called in First Service";
 	}
+
+	@GetMapping("/error/{args}")
+	public String errorMessage(@PathVariable("args") Integer args){
+		return "Argument provided properly";
+	}
+
+	@GetMapping("/error/throw/{throw}")
+	public String ErrorException(@PathVariable("throw") boolean should){
+		if (should) throw new RuntimeException("Thrown from /error/...");
+		return "No message has been thrown";
+	}
+
 }
---
+++ first-service/src/main/java/com/infoworks/controller/GlobalResponseEntityException.java
@@ -0,0 +1,47 @@
+package com.infoworks.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalResponseEntityException extends ResponseEntityExceptionHandler {
+
+    @Override @SuppressWarnings("Duplicates")
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex
+            , HttpHeaders headers
+            , HttpStatus status
+            , WebRequest request) {
+        //
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date().getTime());
+        body.put("status", status.value());
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllKindOfExceptions(
+            Exception ex, WebRequest request) {
+        //
+        List<String> error = new ArrayList<>();
+        error.add(ex.getLocalizedMessage());
+        return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
