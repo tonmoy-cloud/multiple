8191d6e7a246df94dba26526fc088acfeeccf775
Towhidul Islam <m.towhid.islam@gmail.com>
2021-11-25 12:57:10 +0600

    updated with logger and logger-level example

--- first-service/src/main/java/com/infoworks/FirstApplication.java
+++ first-service/src/main/java/com/infoworks/FirstApplication.java
@@ -1,13 +1,29 @@
 package com.infoworks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+
 @SpringBootApplication
-public class FirstApplication {
+public class FirstApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
 	}
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(FirstApplication.class);
+
+	@Override
+	public void run(String... args) throws Exception {
+		String value = "me-wow";
+		LOGGER.trace("doStuff needed more information - {}", value);
+		LOGGER.debug("doStuff needed to debug - {}", value);
+		LOGGER.info("doStuff took input - {}", value);
+		LOGGER.warn("doStuff needed to warn - {}", value);
+		LOGGER.error("doStuff encountered an error with value - {}", value);
+	}
 }
