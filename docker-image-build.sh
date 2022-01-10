506a1401472f7938baf5a35bd8c29f9093d29d56
Towhidul Islam <m.towhid.islam@gmail.com>
2022-01-10 14:23:27 +0600

    Final Push For Image-Build Scripts

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-echo 'Creating First-Service Image'
-docker image build -f first-service/Dockerfile-self -t first-web-service:1.0 ./first-service/
-#docker image push <docker-hub-repository>:first-web-service-1.0
-
-echo 'Creating Second-Service Image'
-docker image build -f second-service/Dockerfile-self -t second-web-service:1.0 ./second-service/
-#docker image push <docker-hub-repository>:second-web-service-1.0
-
-echo 'Creating Auth-Service Image'
-docker image build -f auth-service/Dockerfile-self -t auth-web-service:1.0 ./auth-service/
-#docker image push <docker-hub-repository>:auth-web-service-1.0
-
-echo 'Creating Gateway-Service Image'
-docker image build -f gateway-service-config/Dockerfile-self -t gateway-service:1.0 ./gateway-service-config/
-#docker image push <docker-hub-repository>:gateway-service-1.0
-
-echo 'Creating Monitoring Image'
-docker image build -f monitoring/Dockerfile -t prometheus-db:1.0 ./monitoring/
-#docker image push <docker-hub-repository>:prometheus-db-1.0
+AppVersion="1.0"
+#
+###
+FirstServiceName="first-service"
+echo "Creating ${FirstServiceName} Image"
+docker image build -f ${FirstServiceName}/Dockerfile-self -t ${FirstServiceName}:${AppVersion} ./${FirstServiceName}/
+#docker image push <docker-hub-repository>:${FirstServiceName}-${AppVersion}
+#
+###
+SecondServiceName="second-service"
+echo "Creating ${SecondServiceName} Image"
+docker image build -f ${SecondServiceName}/Dockerfile-self -t ${SecondServiceName}:${AppVersion} ./${SecondServiceName}/
+#docker image push <docker-hub-repository>:${SecondServiceName}-${AppVersion}
+#
+###
+AuthServiceName="auth-service"
+echo "Creating ${AuthServiceName} Image"
+docker image build -f ${AuthServiceName}/Dockerfile-self -t ${AuthServiceName}:${AppVersion} ./${AuthServiceName}/
+#docker image push <docker-hub-repository>:${AuthServiceName}-${AppVersion}
+#
+###
+GatewayServiceDir="gateway-service-config"
+GatewayServiceName="gateway-service"
+echo "Creating ${GatewayServiceName} Image"
+docker image build -f ${GatewayServiceDir}/Dockerfile-self -t ${GatewayServiceName}:${AppVersion} ./${GatewayServiceDir}/
+#docker image push <docker-hub-repository>:${GatewayServiceName}-${AppVersion}
+#
+###
+MonitoringServiceDir="monitoring"
+MonitoringServiceName="prometheus-db"
+echo "Creating ${MonitoringService} Image"
+docker image build -f ${MonitoringServiceDir}/Dockerfile -t ${MonitoringServiceName}:${AppVersion} ./${MonitoringServiceDir}/
+#docker image push <docker-hub-repository>:${MonitoringServiceName}-${AppVersion}
